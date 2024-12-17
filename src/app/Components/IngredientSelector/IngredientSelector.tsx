f801207baf3982e3c59d4a46f9651e531d9cf7b9
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-12-17 22:58:18 +0100

    chore: changed icon dimention and opacity

--- src/app/Components/IngredientSelector/IngredientSelector.tsx
+++ src/app/Components/IngredientSelector/IngredientSelector.tsx
@@ -91,9 +91,9 @@ export default function IngredientSelector({ ingredients, setIngredients, runQue
                         disabled={isLoading}
                     >
                         {isLoading ? (
-                            <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+                            <svg className="animate-spin h-4 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <circle className="" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                <path className="" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
                             </svg>
                         ) : (
                             "Add"
