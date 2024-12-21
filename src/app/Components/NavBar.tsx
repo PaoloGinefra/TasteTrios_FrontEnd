1ab4c57c5398ddb16bd5dff8facfb9cf32ced1d7
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-12-21 20:03:21 +0100

    chore: removed warining

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -71,6 +71,9 @@ export default function NavBar() {
                 <Typography
                     className="mr-4 cursor-pointer py-1.5 font-medium font-bold"
                     variant="h4"
+                    placeholder=""
+                    onPointerEnterCapture={() => { }}
+                    onPointerLeaveCapture={() => { }}
                 >
                     <Link href={"/"}>
                         TASTE TRIOS
